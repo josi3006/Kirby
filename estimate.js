58d9c438613dd7a94a87f1395fad6d6dd923c40d
JGSims <josi3006@hotmail.com>
2020-11-06 19:12:34 -0500

    Rebuilding estimator

--- estimate.js
+++ estimate.js
@@ -1,28 +1,3 @@
-import React, { useState, useEffect } from "react";
-
-
-
-const Estimator = () => {
-
-    var msg = "Hello React!"
-
-
-    return (
-
-        <div>
-            <p>{msg}</p>
-        </div>
-    )
-
-};
-
-export default Estimator;
-
-
-ReactDOM.render(
-    <Estimator />,
-    document.getElementById('estimate-component-goes-here')
-);
 
 
 

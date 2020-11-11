d5c1cc02f36bcdee7c4bffed637a7af0f3b1aa25
JGSims <josi3006@hotmail.com>
2020-11-10 19:09:51 -0500

    Estimate form built

--- estimate.js
+++ estimate.js
@@ -3,3 +3,25 @@ Loggit = () => {
     document.getElementById('dayRadios').setAttribute('disabled', 'disabled');
 
 }
+
+Disabler = (event) => {
+    console.log('Clicked to disable');
+    document.getElementById(event.target).setAttribute('disabled', 'disabled');
+}
+
+// $("fieldset").click(function (event) {
+//     console.log(" was clicked");
+//     // document.getElementById(event.currentTarget.id).setAttribute('disabled', 'disabled');
+
+// });
+
+
+// $("#zipCodeSet").bind('change', function () {
+//     console.log('this works');
+//     $("#zipCodeSet").attr('disabled', 'disabled');
+// });
+
+$("fieldset").bind('change', function () {
+    console.log('this works');
+    $(this).attr('disabled', 'disabled');
+});

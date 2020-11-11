9019c4d4c50c36a773be75c80b1b3920b685ba02
JGSims <josi3006@hotmail.com>
2020-11-11 12:10:37 -0500

    Estimator function generates array of fees

--- estimate.js
+++ estimate.js
@@ -1,13 +1,4 @@
-Loggit = () => {
-    console.log('You clicked me!  Again!!');
-    document.getElementById('dayRadios').setAttribute('disabled', 'disabled');
 
-}
-
-Disabler = (event) => {
-    console.log('Clicked to disable');
-    document.getElementById(event.target).setAttribute('disabled', 'disabled');
-}
 
 // $("fieldset").click(function (event) {
 //     console.log(" was clicked");
@@ -21,7 +12,42 @@ Disabler = (event) => {
 //     $("#zipCodeSet").attr('disabled', 'disabled');
 // });
 
-$("fieldset").bind('change', function () {
-    console.log('this works');
-    $(this).attr('disabled', 'disabled');
+// $("fieldset").bind('change', function () {
+//     console.log('this works');
+//     $(this).attr('disabled', 'disabled');
+// });
+
+
+$("fieldset").bind('submit', function () {
+    $(this).prop('checked', false);
 });
+
+// $("input").bind('click', function () {
+//     console.log(this.value + ' is the value clicked')
+// });
+
+// $("select").bind('click', function () {
+//     console.log(this.value + ' is the value clicked')
+// });
+
+$("select, input").bind('click', function () {
+    console.log(this.value + ' is the value clicked')
+});
+
+
+
+
+
+$(document).ready(function() {
+    $("button").click(function(){
+
+        var feeArray = [];
+
+        $.each($("input:checked"), function(){
+            feeArray.push($(this).val());
+        });
+
+        console.log('Array is: ' + feeArray);
+    });
+});
+

8041b408b545fc13aec9c215cc29ea144ab16f8d
JGSims <josi3006@hotmail.com>
2021-01-01 16:40:53 -0500

    Contact form working

---
+++ send-mail.php
@@ -0,0 +1,40 @@
+<?php
+    $mail_to = 'josi3006@hotmail.com'; // specify your email here
+
+    // Assigning data from the $_POST array to variables
+    $name = $_POST['sender_name'];
+    $mail_from = $_POST['sender_email'];
+    $phone = $_POST['sender_phone'];
+    $message = $_POST['sender_message'];
+
+    // Construct email subject
+    $subject = 'K I R B Y contact from ' . $name;
+
+    // Construct email body
+    $body_message = 'From: ' . $name . "\r\n";
+    $body_message .= 'E-mail: ' . $mail_from . "\r\n";
+    $body_message .= 'Phone: ' . $phone . "\r\n";
+    $body_message .= 'Message: ' . $message;
+
+    // Construct email headers
+    $headers = 'From: ' . $mail_from . "\r\n";
+    $headers .= 'Reply-To: ' . $mail_from . "\r\n";
+
+    ini_set('SMTP', 'smtp.gmail.com');
+    ini_set('smtp_port', '587');
+
+    $mail_sent = mail($mail_to, $subject, $body_message, $headers);
+
+    if ($mail_sent == true){ ?>
+        <script language="javascript" type="text/javascript">
+        alert('Thank you for the message. We will contact you shortly.');
+        window.location = 'landing.html';
+        </script>
+    <?php } else { ?>
+    <script language="javascript" type="text/javascript">
+        alert('Message not sent, mfr. Please notify the site administrator admin@admin.com');
+        window.location = 'classes.html';
+    </script>
+    <?php
+    }
+?>
